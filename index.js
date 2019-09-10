78bbdf47797a000f5d1b4dfc7be6c275f19f047d
Connie Lai <connielion011@gmail.com>
2019-09-10 02:53:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // add solution here
+function theBeatlesPlay(musArr, instrArr){
+  const res = [];
+  for(let i = 0; i < musArr.length; i++){
+    let instrument = instrArr[i];
+    let name = musArr[i];
+    res.push(`${name} plays ${instrument}`);
+  }
+  return res;
+}
+
+const johnLennonFacts = factsArr => {
+  let i = 0;
+  const res = [];
+  while(i<factsArr.length){
+    let fact = factsArr[i];
+    res.push(fact+'!!!');
+  }
+  return res;
+}
